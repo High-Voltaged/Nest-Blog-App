0690081c3bb025830e3330c491238990e7923eb6
Polina Balazhy <lefugdot@gmail.com>
2023-04-04 16:35:17 +0300

    add server config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { WinstonModule } from 'nest-winston';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
 import { PostsService } from './posts/posts.service';
-import loggerOptions from './logger';
+import loggerOptions from './config/logger.config';
+import databaseConfig from './config/server.config';
 
 @Module({
-  imports: [WinstonModule.forRoot(loggerOptions)],
+  imports: [
+    WinstonModule.forRoot(loggerOptions),
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        DATABASE_HOST: Joi.string().default('localhost'),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+      },
+    }),
+  ],
   controllers: [AppController, PostsController],
   providers: [AppService, PostsService],
 })
