ccd9987e867985200394e26699aa3dcd3a67184b
Polina Balazhy <lefugdot@gmail.com>
2023-04-05 23:20:09 +0300

    remove redux persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,21 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 import App from '~/App';
-import Loader from '~/components/Loader/Loader';
-import { persistor, store } from '~/store/store';
+import { store } from '~/store/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={<Loader />} persistor={persistor}>
-        <ChakraProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </ChakraProvider>
-      </PersistGate>
+      <ChakraProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>,
 );
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,39 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
-import {
-  persistReducer,
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 import { apiSlice } from './api/api-slice';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: [apiSlice.reducerPath],
-};
-
-const persistedReducer = persistReducer(
-  persistConfig,
-  combineReducers({
+export const store = configureStore({
+  reducer: combineReducers({
     [apiSlice.reducerPath]: apiSlice.reducer,
   }),
-);
-
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware: any) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }).concat(apiSlice.middleware),
+  middleware: (getDefaultMiddleware: any) => getDefaultMiddleware({}).concat(apiSlice.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export const persistor = persistStore(store);
